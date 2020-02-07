de62714eb650b3fd1e8540afb4acc77ab3b826df
darcikole <53584516+darcikole@users.noreply.github.com>
2020-02-07 08:38:10 -0800

    change smooth scroll js

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -1,10 +1,15 @@
 // smooth scroll to all links
-$(document).ready(function(){
-    $( "a" ).click(function( event ) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 1000);
-    });
-});
+$('a[href*="#"]').on('click', function(e) {
+    e.preventDefault()
+  
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top,
+      },
+      500,
+      'linear'
+    )
+  })
 
 // contact form open/close //
 function openForm() {
